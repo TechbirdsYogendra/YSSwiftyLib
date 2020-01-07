4df0164acbc36bdc6d25095d39ae8231f89c82ff
Yogendra Singh <>
2020-01-07 11:14:25 +0530

    Initial Setup

---
+++ YSSwiftyLib/YSSwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  YSSwiftyLib.h
+//  YSSwiftyLib
+//
+//  Created by Yogendra Singh on 07/01/20.
+//  Copyright © 2020 Yogendra Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSSwiftyLib.
+FOUNDATION_EXPORT double YSSwiftyLibVersionNumber;
+
+//! Project version string for YSSwiftyLib.
+FOUNDATION_EXPORT const unsigned char YSSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSSwiftyLib/PublicHeader.h>
+
+
